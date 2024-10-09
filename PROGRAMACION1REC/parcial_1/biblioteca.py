59eff352f9899d8da0e84772365486f1c1afcd07
DaroMateo <daromateo@gmail.com>
2024-10-08 21:27:45 -0300

    con argumentacion

--- PROGRAMACION1REC/parcial_1/biblioteca.py
+++ PROGRAMACION1REC/parcial_1/biblioteca.py
@@ -2,6 +2,15 @@ import random
 
 # Función para generar una lista alfanumérica de 1000 elementos usando códigos ASCII
 def generar_lista_alfanumerica():
+    """
+    Genera una lista alfanumérica de 1000 elementos utilizando códigos ASCII. La lista
+    contiene 26 caracteres alfabéticos mayúsculas (A-Z) y 10 caracteres numéricos (0-9)
+    en orden aleatorio.
+    
+    Returns:
+    list: una lista alfanumérica de 1000 elementos.
+    """
+    
     lista = []
     for _ in range(1000):
         num = random.randint(0, 35)
@@ -13,6 +22,17 @@ def generar_lista_alfanumerica():
 
 # Función para ordenar una lista alfanumérica
 def ordenar_lista(lista, criterio='ASC'):
+    """
+    Ordena una lista alfanumérica en forma ascendente o descendente
+    
+    Args:
+        lista (list): La lista alfanumérica a ordenar.
+        criterio (str): El criterio de ordenamiento. Puede ser 'ASC' para ordenar en forma ascendente o 'DESC' para ordenar en forma descendente. Por defecto es 'ASC'.
+    
+    Returns:
+        list: La lista ordenada seg n el criterio seleccionado.
+    """
+    
     for i in range(len(lista)):
         for j in range(i + 1, len(lista)):
             if (criterio == 'ASC' and lista[i] > lista[j]) or (criterio == 'DESC' and lista[i] < lista[j]):
@@ -22,11 +42,24 @@ def ordenar_lista(lista, criterio='ASC'):
     return lista
 
 def mostrar_lista(lista):
+    """
+    Muestra los elementos de una lista.
+    
+    Parameters:
+    lista (list): la lista a mostrar
+    """
+    
     for elem in lista:
         print(elem, end=" ")
     print()
 
 def mostrar_matriz(matriz):
+    """
+    Muestra por pantalla una matriz de enteros, con los elementos alineados en columnas.
+    
+    Parameters:
+    matriz (list): La matriz a mostrar.
+    """
     for i in range(len(matriz)):
         for j in range(len(matriz[i])):
             print(matriz[i][j], end=" ")
@@ -34,6 +67,16 @@ def mostrar_matriz(matriz):
 
 # Función para contar la cantidad de cada carácter alfabético
 def contar_caracteres(lista):
+    """
+    Crea un diccionario que contiene como clave cada carácter alfabético mayúscula
+    y como valor la cantidad de veces que se repite en la lista.
+    
+    Parameters:
+    lista (list): lista de caracteres alfanuméricos
+    
+    Returns:
+    dict: diccionario con la cantidad de veces que se repite cada carácter alfabético
+    """
     conteo = {}
     # Inicializar el diccionario con todas las letras del alfabeto en mayúsculas
     for i in range(65, 91):  # Códigos ASCII de 'A' a 'Z'
@@ -49,6 +92,17 @@ def contar_caracteres(lista):
 # Función para obtener el caracter que más y menos se repite
 def obtener_max_min(conteo):
     # Inicializar valores máximos y mínimos
+    """
+    Obtiene el caracter que más y menos se repite en un diccionario de conteo.
+    
+    Parameters:
+    conteo (dict): Diccionario que contiene como clave cada carácter alfabético
+                   y como valor la cantidad de veces que se repite.
+    
+    Returns:
+    tuple: Tupla que contiene el caracter que más se repite, la cantidad de veces que se repite,
+           el caracter que menos se repite y la cantidad de veces que se repite.
+    """
     max_caracter = None
     min_caracter = None
     max_cantidad = -1
@@ -68,6 +122,12 @@ def obtener_max_min(conteo):
 
 # Función para generar una matriz de 10x10 de números enteros
 def generar_matriz():
+    """
+    Genera una matriz de 10x10 de números enteros aleatorios entre 0 y 100.
+    
+    Returns:
+    list: La matriz generada.
+    """
     matriz = []
     for i in range(10):
         fila = [0] * 10  # Crear una fila con 10 elementos inicializados a 0
@@ -78,6 +138,19 @@ def generar_matriz():
 
 # Función para validar el ingreso de un número entero de dos dígitos como mínimo
 def validar_ingreso_entero(min_digitos=2):
+    """
+    Valida el ingreso de un número entero de dos dígitos como mínimo.
+    
+    Parameters:
+    min_digitos (int): La cantidad mínima de dígitos que debe tener el número entero.
+    
+    Returns:
+    str: La entrada del usuario como una cadena de caracteres.
+    
+    Notes:
+    Verifica que la entrada tenga la cantidad mínima de caracteres y que cada carácter sea un dígito.
+    Si no es válido, pide al usuario que ingrese nuevamente una secuencia numérica.
+    """
     while True:
         entrada = input("Ingrese una secuencia numérica (de dos dígitos como mínimo): ")
         es_valido = True
@@ -100,6 +173,16 @@ def validar_ingreso_entero(min_digitos=2):
 
 # Función para buscar una secuencia numérica en la matriz de manera horizontal
 def buscar_secuencia(matriz, secuencia):
+    """
+    Busca una secuencia numérica en una matriz de manera horizontal.
+    
+    Parameters:
+    matriz (list): La matriz a buscar.
+    secuencia (str): La secuencia numérica a buscar.
+    
+    Returns:
+    bool: True si la secuencia se encuentra en la matriz, False si no.
+    """
     for fila in matriz:
         fila_str = ''
         for num in fila:
@@ -116,7 +199,15 @@ def buscar_secuencia(matriz, secuencia):
             return True
     return False
 
-def mnostrar_secuencia_en_matriz(matriz, secuencia):
+def mostrar_secuencia_en_matriz(matriz, secuencia):
+    """
+    Muestra la secuencia en la matriz, representando los elementos que se encuentran en la
+    secuencia con 'X' y los que no con '0'.
+    
+    Parameters:
+    matriz (list): La matriz a imprimir.
+    secuencia (str): La secuencia numérica a buscar en la matriz.
+    """
     for i in range(len(matriz)):
         for j in range(len(matriz[i])):
             if str(matriz[i][j]) in secuencia:
@@ -127,6 +218,20 @@ def mnostrar_secuencia_en_matriz(matriz, secuencia):
 
 # Menú de opciones
 def menu():
+    """
+    Menú de opciones para interactuar con la lista alfanumérica y la matriz de números enteros.
+    
+    Las opciones permiten:
+        1 - Generar la lista alfanumérica aleatoria.
+        2 - Ordenar la lista alfanumérica.
+        3 - Buscar e informar cuantas veces se repite cada carácter alfabético.
+        4 - Obtener el carácter que más y menos se repite.
+        5 - Generar la matriz aleatoria de números enteros.
+        6 - Buscar una secuencia numérica en la matriz.
+        7 - Salir.
+    
+    Se debe generar la lista alfanumérica y la matriz aleatoria de números enteros antes de acceder a las demás opciones.
+    """
     lista_alfanumerica = []
     matriz = []
     
@@ -189,7 +294,7 @@ def menu():
                     secuencia = validar_ingreso_entero()
                     if buscar_secuencia(matriz, secuencia):
                         print(f"La secuencia numérica {secuencia} existe en la matriz.")
-                        mnostrar_secuencia_en_matriz(matriz, secuencia)
+                        mostrar_secuencia_en_matriz(matriz, secuencia)
                     else:
                         print(f"La secuencia numérica {secuencia} no existe en la matriz.")
             case '7':
